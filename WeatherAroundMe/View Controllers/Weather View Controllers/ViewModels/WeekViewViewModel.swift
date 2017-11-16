81fdbfe59b76af932bac54776dc6f8cb7c0b8edd
Stanislav Redreiev <moderator.promo@gmail.com>
2017-11-16 18:24:34 +0200

    added getter method to return temperature for specific day; added helper that convert temperature value to string.

--- WeatherAroundMe/View Controllers/Weather View Controllers/ViewModels/WeekViewViewModel.swift
+++ WeatherAroundMe/View Controllers/Weather View Controllers/ViewModels/WeekViewViewModel.swift
@@ -11,6 +11,7 @@ import UIKit
 struct WeekViewViewModel {
  
     let weatherData: [WeatherDayData]
+    private let dateFormatter = DateFormatter()
     
     var numberOfSections: Int {
         return 1
@@ -20,20 +21,43 @@ struct WeekViewViewModel {
         return weatherData.count
     }
     
+    /** Day name for a specific day index. */
     func day(for index: Int) -> String {
         
         let weatherDayData = weatherData[index]
-        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
         
         return dateFormatter.string(from: weatherDayData.time)
     }
     
+    /** Date for a specific day index. */
     func date(for index: Int) -> String {
+        
         let weatherDayData = weatherData[index]
-        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM d"
         
         return dateFormatter.string(from: weatherDayData.time)
+    }
+    
+    /** Temperature range for a specific day index. */
+    func temperature(for index: Int) -> String {
+        
+        let weatherDayDate = weatherData[index]
+        let minTemperature = format(temperature: weatherDayDate.temperatureMin)
+        let maxTemperature = format(temperature: weatherDayDate.temperatureMax)
+        
+        return "\(minTemperature) - \(maxTemperature)"
+    }
+    
+    // MARK: - Helpers
+    
+    /** Converts temperature value to specific text format using selected temperature type settings. */
+    private func format(temperature: Double) -> String {
+        switch UserDefaults.temperatureNotation() {
+            case .fahrenheit:
+                return String(format: "%.0f 째F", temperature)
+            case .celsius:
+                return String(format: "%.0f 째C", temperature.toCelcius())
+        }
     }
 }
